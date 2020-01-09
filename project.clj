9b70786745410a3a4d61378affccbed2bfc02ef4
ningDr <ning.dr@foxmail.com>
2020-01-09 10:06:34 +0800

    backup

--- project.clj
+++ project.clj
@@ -5,13 +5,23 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
-                 [com.datomic/datomic-free "0.9.5697"]]
-  :repl-options {:timeout 3000000}
+                 [com.datomic/datomic-free "0.9.5697"]
+                 ;; 音乐库
+                 [overtone "0.10.6"]
+                 ;; http
+                 [clj-http "3.10.0"]
+                 ;; http-kit
+                 [http-kit "2.3.0"]
+                 ;; RSS
+                 [org.clojars.scsibug/feedparser-clj "0.4.0"]
+                 ;; email
+                 [com.draines/postal "2.0.3"]
+                 ]
+  :repl-options {:timeout 30000}
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler clojure4j.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.2"]]}}
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring/ring-mock "0.3.2"]]}}
   :user {:plugins             [[lein-kibit "0.1.8"]
                                [lein-cljfmt "0.6.6"]
                                [cider/cider-nrepl "0.22.4"]]
